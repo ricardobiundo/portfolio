b8f9b3d635707c1db521fee50dc29f594676fda5
ricardobiundo <ricardobiundo@gmail.com>
2022-08-20 06:12:46 +0200

    Imported fontawesome into the app

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
@@ -6,5 +7,6 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  iconfont: 'fa',
   render: h => h(App)
 }).$mount('#app')
