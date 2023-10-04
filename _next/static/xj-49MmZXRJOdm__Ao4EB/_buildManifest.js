bf87693a61e1cc00bffca596e60bd98764267428
CI <CI@example.com>
2023-10-04 12:16:20 +0000

    deploy

---
+++ _next/static/xj-49MmZXRJOdm__Ao4EB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/158-2474a7a402cd2fe16de2.js","static/chunks/pages/index-d984777832346e3e19fa.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
